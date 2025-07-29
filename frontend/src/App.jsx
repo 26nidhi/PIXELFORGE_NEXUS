7686e6b43339f5b1363f5f89a10cad84e12612f7
Nidhi Walke <bcs_2023041@iiitm.ac.in>
2025-07-30 00:07:16 +0530

    created app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import DashboardPage from "./pages/DashboardPage";
+import ProjectDetailsPage from "./pages/ProjectDetailsPage";
+import SettingsPage from "./pages/SettingsPage";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/project/:id" element={<ProjectDetailsPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
