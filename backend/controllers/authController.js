8d3ab463e8974665303b7107380cdb8f7a0b738d
Nidhi Walke <bcs_2023041@iiitm.ac.in>
2025-07-30 12:29:09 +0530

    added auth controller

---
+++ backend/controllers/authController.js
@@ -0,0 +1,46 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+// Register (Admin only)
+export const registerUser = async (req, res) => {
+  const { name, email, password, role } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role,
+    });
+    res.status(201).json({ message: "User registered", user });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Login
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.json({
+      token,
+      user: { id: user._id, name: user.name, role: user.role },
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
